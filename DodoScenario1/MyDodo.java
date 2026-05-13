d0e8cf00c652adc7f3dc5e4a37bfca102da70089
DoakesdextermothaF <1217945@student.roc-nijmegen.nl>
2026-05-13 08:59:14 +0200

    Opdracht jump()
    Bij deze opdracht geeft ik een getal mee (distance), en Dodo loopt precies dat aantal stappen. Na elke stap print het "moved" + het stapnummer naar de console dat wordt uitgeprint.

--- DodoScenario1/MyDodo.java
+++ DodoScenario1/MyDodo.java
@@ -91,10 +91,9 @@ public class MyDodo extends Dodo
         while ( nrStepsTaken < distance ) { // check if more steps must be taken  
             move();                         // take a step
             nrStepsTaken++;                 // increment the counter
+            System.out.println(nrStepsTaken + "moved");
         }
     }
-
-    
     
     
     /**
