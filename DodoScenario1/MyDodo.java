ca3234282a7e65ceae30691314362a7bdbce0206
DoakesdextermothaF <1217945@student.roc-nijmegen.nl>
2026-05-27 09:51:17 +0200

    Opdracht lastiger maze
    In deze opdracht heb de naam veranderd van de methode eenvoudigeDoolhof naar goFindNestInMaze. want de functie werkt ook met de lastiger doolhofs dat was getest.

--- DodoScenario1/MyDodo.java
+++ DodoScenario1/MyDodo.java
@@ -283,7 +283,7 @@ public class MyDodo extends Dodo
     /**
      * This Methode makes the Dodo go through the route of a maze to the nest.
      */
-    public void eenvoudigDoolhof() {
+    public void goFindNestInMaze() {
         while (!onNest()) {
             turnRight();
             while(fenceAhead()) {
